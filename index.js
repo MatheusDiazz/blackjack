4211e6dd817648cb0b177f0998e0d0f45543aec7
Matheus Diaz <matheusdz155@gmail.com>
2025-06-18 12:52:04 -0400

    Played with logic for the game

---
+++ index.js
@@ -0,0 +1,20 @@
+let firstCard = 10
+let secondCard = 12
+let sum = firstCard + secondCard
+let hasBlackJack = false
+let isAlive = true
+let msg = ""
+if (sum<=20) {
+    msg = "Do you want to draw another card?"
+}
+else if (sum ===21) {
+    hasBlackJack = true
+    msg = "Wohoo, you've got Blackjack!"
+}
+else {
+    msg = "You're out of the game" 
+    isAlive = false
+}
+console.log(hasBlackJack)
+console.log(isAlive)
+console.log(msg)
