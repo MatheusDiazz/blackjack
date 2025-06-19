e8a16f1b35447000ef8d4e095d9d6bcc75edce93
Matheus Diaz <matheusdz155@gmail.com>
2025-06-18 21:14:46 -0400

    Start button now shows the two card numbers

--- index.js
+++ index.js
@@ -6,19 +6,18 @@ let isAlive = true
 let msg = ""
 let messageEl = document.getElementById("message-el")
 let sum = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 function startGame() {
-    if (sum<=20) {
-    msg = "Do you want to draw another card?"
+    cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+    if (cardSum <= 20) {
+        msg = "Do you want to draw another card?"
+    } else if (cardSum === 21) {
+        hasBlackJack = true
+        msg = "Wohoo, you've got Blackjack!"
+    } else {
+        msg = "You're out of the game"
+        isAlive = false
+    }
+    messageEl.textContent = msg
+    sum.textContent = "Sum: " + cardSum
 }
-else if (sum ===21) {
-    hasBlackJack = true
-    msg = "Wohoo, you've got Blackjack!"
-}
-else {
-    msg = "You're out of the game" 
-    isAlive = false
-}
-messageEl.textContent = msg
-sum.textContent += " " + cardSum
-}
-
